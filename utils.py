01866bdb13398ebde0682d38ceac5e7b5efbd41e
ElieSan99 <eliegislainsanon@gmail.com>
2024-12-14 12:09:57 +0100

    drunet model

---
+++ utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+
+
+def test_pad(model, L, modulo=16):
+    h, w = L.size()[-2:]
+    paddingBottom = int(np.ceil(h/modulo)*modulo-h)
+    paddingRight = int(np.ceil(w/modulo)*modulo-w)
+    L = torch.nn.ReplicationPad2d((0, paddingRight, 0, paddingBottom))(L)
+    E = model(L)
+    E = E[..., :h, :w]
+    return E
+
+
+def test_onesplit(model, L, refield=32, min_size=256, sf=1, modulo=1):
+    '''
+    model:
+    L: input Low-quality image
+    refield: effective receptive filed of the network, 32 is enough
+    min_size: min_sizeXmin_size image, e.g., 256X256 image
+    sf: scale factor for super-resolution, otherwise 1
+    modulo: 1 if split
+    '''
+    h, w = L.size()[-2:]
+
+    top = slice(0, (h//2//refield+1)*refield)
+    bottom = slice(h - (h//2//refield+1)*refield, h)
+    left = slice(0, (w//2//refield+1)*refield)
+    right = slice(w - (w//2//refield+1)*refield, w)
+    Ls = [L[..., top, left], L[..., top, right], L[..., bottom, left], L[..., bottom, right]]
+    Es = [model(Ls[i]) for i in range(4)]
+    b, c = Es[0].size()[:2]
+    E = torch.zeros(b, c, sf * h, sf * w).type_as(L)
+    E[..., :h//2*sf, :w//2*sf] = Es[0][..., :h//2*sf, :w//2*sf]
+    E[..., :h//2*sf, w//2*sf:w*sf] = Es[1][..., :h//2*sf, (-w + w//2)*sf:]
+    E[..., h//2*sf:h*sf, :w//2*sf] = Es[2][..., (-h + h//2)*sf:, :w//2*sf]
+    E[..., h//2*sf:h*sf, w//2*sf:w*sf] = Es[3][..., (-h + h//2)*sf:, (-w + w//2)*sf:]
+    return E
